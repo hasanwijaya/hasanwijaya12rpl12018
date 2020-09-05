57f6791739497f39af9526bfcdeacface28f3418
Hasan Wijaya <hasanwijaya.xrpl2018smkrus@gmail.com>
2020-09-05 19:21:40 +0700

    splashscreen

--- app/src/main/java/com/example/task/activity/SplashScreenActivity.java
+++ app/src/main/java/com/example/task/activity/SplashScreenActivity.java
@@ -2,15 +2,42 @@ package com.example.task.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.task.R;
 
+import static android.os.Build.ID;
+
 public class SplashScreenActivity extends AppCompatActivity {
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String ID = "id";
+    private int waktu_loading = 300;
+    private String id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        id = sharedPreferences.getString(ID, "");
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (id.isEmpty()) {
+                    Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Intent intent = new Intent(SplashScreenActivity.this, DashboardActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }, waktu_loading);
     }
 }
